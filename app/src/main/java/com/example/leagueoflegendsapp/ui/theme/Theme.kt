c9fff295059a754e0b3ed0fabf95f5c57b50f551
NgaHQ <ngahq@teamsolutions.vn>
2025-06-26 10:17:26 +0700

    Implement : Theme app

--- app/src/main/java/com/example/leagueoflegendsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/leagueoflegendsapp/ui/theme/Theme.kt
@@ -1,58 +1,39 @@
 package com.example.leagueoflegendsapp.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+val LightColorTheme = lightColorScheme(
+    background = Background
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+val DarkColorTheme = lightColorScheme(
+    background = BackgroundDark
 )
 
 @Composable
-fun LeagueOfLegendsAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+fun extendedColor(light: Color, dark: Color): Color {
+    return if (isSystemInDarkTheme()) dark else light
+}
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+val ColorScheme.extraColor: Color
+    @Composable get() = extendedColor(Color.White, Color.Black)
+
+val shapes = Shapes(extraSmall = RoundedCornerShape(5.dp), medium = RoundedCornerShape(15.dp))
+
+@Composable
+fun LeagueOfLegendsAppTheme(content: @Composable () -> Unit) {
+    val theme = if (isSystemInDarkTheme()) {
+        DarkColorTheme
+    } else {
+        LightColorTheme
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    MaterialTheme(colorScheme = theme, content = content, typography = Typography, shapes = shapes)
 }
--- app/src/main/java/com/example/leagueoflegendsapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/leagueoflegendsapp/ui/theme/Type.kt
@@ -1,34 +1,22 @@
 package com.example.leagueoflegendsapp.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val Typography: Typography
+    @Composable get() = Typography(
+        bodyLarge = TextStyle(
+            fontWeight = FontWeight.Normal, fontSize = 17.sp, lineHeight = 24.sp
+        ), bodyMedium = TextStyle(
+            fontWeight = FontWeight.Medium, fontSize = 15.sp, lineHeight = 20.sp
+        ), bodySmall = TextStyle(
+            fontWeight = FontWeight.Normal, fontSize = 15.sp, lineHeight = 24.sp
+        ), titleLarge = TextStyle(
+            fontWeight = FontWeight.Bold, fontSize = 32.sp, lineHeight = 36.sp
+        ), titleSmall = TextStyle(
+            fontWeight = FontWeight.Medium, fontSize = 17.sp, lineHeight = 24.sp
+        )
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
